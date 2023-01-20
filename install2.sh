ebc6034210c1e3564f0b1ee978d7ef61954c07cd
Jitender Yadav <89233722+jitender1812@users.noreply.github.com>
2023-01-20 11:59:59 +0530

    Add files via upload

---
+++ install2.sh
@@ -0,0 +1,6 @@
+pip3 install numpy==1.19.2
+pip3 install tensorflow-2.6.0-cp39-cp39-linux_aarch64.whl
+pip3 install cvlib
+pip3 install keras==2.6.0
+apt-get install libatlas-base-dev -y
+wget https://drive.google.com/file/d/1XI0YUnFa3t6O6lDDFI5s2RgMJsmjNz1y/view?usp=share_link
